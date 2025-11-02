733e4d672ad06cccfa2a455b18e0d64fb29d69b5
adilsonkassopa <adilsonkassopa@gmail.com>
2025-11-03 00:06:52 +0100

    fiz alterações na configuração do cors, não tinha configurado o cors então coloquei novas configurações

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ const app = express()
 import { userRouter } from './modules/users/routes.ts'
 import cors from "cors";
 
-app.use(cors())
+const corsOption = {
+    origin:'*',
+    methods:'GET,POST,PUT,DELETE',
+    allowedHeaders:'Content-Type,Authorization'
+}
+
+
 app.use(express.json())
+app.use(cors(corsOption))
 app.use("/users",userRouter)
 
 app.listen(3000, () => console.log('the server is running in port 3000'))
